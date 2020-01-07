79b19609cf5198347d98dd370e3ca32f8934f328
Trevor Power <trevordepaor@gmail.com>
2020-01-07 17:31:18 +0000

    print errors on initial ps1 run

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const run = require('child_process').spawnSync;
 const spawn = require('child_process').spawn;
 const gulp = require('gulp');
+const colors = require('ansi-colors');
+const log = require('fancy-log');
 
 const switches = [
    '-NoProfile',
@@ -12,17 +14,25 @@ const switches = [
 ];
 
 module.exports = function (file) {
-   const initResult = run('powershell', switches.concat(file)).stdout;
-   const tasks = JSON.parse(initResult);
-   Object.keys(tasks).forEach(function(key) {
-      const cb = (cb) => {
-         const execSwitches = switches.concat(file, key);
-         const taskProcess = spawn('powershell', execSwitches, {stdio: ['inherit', 'pipe']});
 
-         taskProcess.stdout.on('data', (data) => console.log(JSON.parse(data.toString())));
+   log('Importing Tasks', colors.magenta(file));
 
-         taskProcess.on('close', () => cb());
-      };
-      gulp.task(key, tasks[key], cb);
-   });
+   const result = run('powershell', switches.concat(file));
+
+   if (result.stderr.length > 0)
+      log.error(result.stderr.toString());
+   else {
+      const tasks = JSON.parse(result.stdout);
+      Object.keys(tasks).forEach(function (key) {
+         const cb = (cb) => {
+            const execSwitches = switches.concat(file, key);
+            const taskProcess = spawn('powershell', execSwitches, { stdio: ['inherit', 'pipe'] });
+
+            taskProcess.stdout.on('data', (data) => console.log(JSON.parse(data.toString())));
+
+            taskProcess.on('close', () => cb());
+         };
+         gulp.task(key, tasks[key], cb);
+      });
+   }
 };
